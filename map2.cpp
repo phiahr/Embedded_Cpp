ffaf27d877060ba80976a5c2a3e09621ed90bc59
Philipp Ahrendt <pipo.ahrendt@gmail.com>
2023-02-08 00:02:43 +0100

    Removed dead code for map2.cpp

--- map2.cpp
+++ map2.cpp
@@ -28,39 +28,6 @@ int main(int argc, char **argv)
         std::cin >> qin;
 
         if (qin == "END") break;
-
-        // auto findResult = std::find_if(std::begin(hMap), std::end(hMap), [qin](const std::pair<std::string, double> pair)
-        // {
-        //     // return pair
-        //     // std::cout << pair.first << std::endl;
-        //     // sanity check before 
-        //     // double input;
-        //     // if (qin == pair.first)
-        //     // {
-        //     //     std::cout << "value[" << pair.first << "]= " << pair.second << std::endl;
-        //     //     return true;
-        //     // }
-        //     // else {
-        //         try
-        //         {
-        //             // return pair.second == std::stod(qin);
-        //             // if (pair.second+pair.second*0.01 == (int)std::stod(qin)/10){
-        //             double v = std::stod(qin);
-        //             if (pair.second > v-0.01*v && pair.second < v+0.01*v){
-        //                 std::cout << "value[" << pair.first << "]= " << pair.second << std::endl; 
-        //                 // std::cout << pair.second << std::endl;
-        //                 // std::cout << pair.first << std::endl;
-        //             }
-        //         }
-        //         catch(std::exception &err)
-        //         {
-        //             // std::cout << "This ID does not exist" << std::endl;
-        //             return false;
-        //         }
-        //     // }
-            
-        //     return false;
-        // });
         if (qin[0] == '+')
         {
             double v;
@@ -84,25 +51,20 @@ int main(int argc, char **argv)
             catch(const std::exception& e)
             {
                 std::cerr << "Invalid Value" << '\n';
-                // std::cout << "This ID does not exist" << std::endl;
-
             }
         }
         else {
         if (hMap.find(qin) != hMap.end())
         {
             // found
-            // std::cout << "Found" << std::endl;
             std::cout << "value[" << qin << "]= " << hMap[qin] << std::endl; 
         }
-        // else if (findResult != std::end(hMap)) {/*std::cout << "found value: " << findResult->first << std::endl;*/}
         else
         {
             // not found
             std::cout << "This ID does not exist" << std::endl;
         }
         }
-        // if (findResult != hMap.end())
         
     }
     std::cout << "Bye..." << std::endl;
