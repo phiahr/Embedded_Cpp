fddcfb9d1121d4f4a9e443cefc95b6da8448e362
Philipp Ahrendt <pipo.ahrendt@gmail.com>
2023-02-07 13:00:22 +0100

    Used setw() instead of own function

--- histogram.cpp
+++ histogram.cpp
@@ -6,60 +6,12 @@
 #include <map>
 #include <cmath>
 
-using std::string;
-using std::vector;
-
-
-void printResult(vector<double>& ans)
-{
- 
-    // Traverse the vector ans
-    for (auto& it : ans) {
- 
-        // Print elements
-        std::cout << it << ' ';
-    }
-    std::cout << "\n";
-}
-
-int countElementsInSlice(vector<double>& arr,
-                    int X, int Y)
-{
- 
-    // Starting and Ending iterators
-    auto start = arr.begin() + X;
-    auto end = arr.begin() + Y + 1;
- 
-    // To store the sliced vector
-    vector<double> result(Y - X + 1);
- 
-    // Copy vector using copy function()
-    copy(start, end, result.begin());
- 
-    // Return the final sliced vector
-    // return result;
-
-    // return count
-    printResult(result);
-    return result.size();
-}
-
-int count_digit(int number) {
-   return number == 0 ? 1 : int(log10(number) + 1);
-}
-
-int getWhiteSpaces(int number, int maxNumber)
-{
-  return (count_digit(maxNumber)-count_digit(number));
-}
-
-
 int main(int argc, char **argv) {
-  string file_name = argv[1];
-  vector<double> buf;
+  std::string file_name = argv[1];
+  std::vector<double> buf;
   std::ifstream fin(file_name, std::ios::in);
 
-  string line;
+  std::string line;
   auto mean = 0.0;
 
   while (std::getline(fin, line)) {
@@ -111,7 +63,7 @@ int main(int argc, char **argv) {
     // number of stars is proportional to the largest number having 60 stars
     int starsCount = ((float)buckets[i] / (float)*largestBucket)*60;
 
-    // getWhiteSpaces() to right align the numbers that have less digits than the maximum numbers 
-    std::cout << std::string(getWhiteSpaces(i*100, buckets.size()*100), ' ') << i*100 << "\t" << std::string(getWhiteSpaces(buckets[i], *largestBucket), ' ') << buckets[i] << " " << std::string(starsCount, '*') << std::endl;
+    // set width to right align numbers 
+    std::cout << std::setw(4) << i*100 << std::setw(8) << buckets[i] << " " << std::string(starsCount, '*') << std::endl;
   }
 }
